928114b3e297b5ed405a0f10e20e89f9a5eb4bba
DrTeePot <tristanpotter@rogers.com>
2015-01-31 14:54:34 -0500

    added boilerplate for default tangent

--- tangent/views.py
+++ tangent/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'tangent/base.html')
+    return render(request, 'tangent/index.html')
