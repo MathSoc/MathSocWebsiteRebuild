0b1e7f9a332b341d4ee86be3bb9f6f25c2d651fd
DrTeePot <tristanpotter@rogers.com>
2015-01-31 03:04:36 -0500

    added services backend stuff

--- services/models.py
+++ services/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Locker(models.Model):
+    owner = models.ForeignKey(User)
+
+    locker_number = models.IntegerField(unique=True)
+    current_combo = models.CharField(max_length=6)
+    combo_number = models.IntegerField()
+
+    combo0 = models.CharField(max_length=6)
+    combo1 = models.CharField(max_length=6)
+    combo2 = models.CharField(max_length=6)
+    combo3 = models.CharField(max_length=6)
+    combo4 = models.CharField(max_length=6)
+
+    def cycle_combo(self):
+        self.current_combo += 1
+        num = self.combo_number
+        self.set_combo(num)
+
+    def set_combo(self, num):
+        if num == 0:
+            self.combo_number = self.combo0
+        elif num == 1:
+            self.combo_number = self.combo1
+        elif num == 2:
+            self.combo_number = self.combo2
+        elif num == 3:
+            self.combo_number = self.combo3
+        else:  # num == 4 or more
+            self.combo_number = self.combo4
+
+
+class Exam(models.Model):
+    name = models.CharField(max_length=256)
+    course_number = models.IntegerField(max_length=3)
+    subject = models.CharField(max_length=10)
+
+    file = models.FileField()
