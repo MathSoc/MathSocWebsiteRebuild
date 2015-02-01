eff0e63c62bf1ab8bd58941eb72f34bf21a1b4a2
DrTeePot <tristanpotter@rogers.com>
2015-02-01 06:43:38 -0500

    added position pages and minor updated to others

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('frontend.views',
                        url(r'^organization/(?P<org_id>\d+)/', 'organization', name='frontend_organization'),
                        url(r'^office/', 'office', name='frontend_office'),
                        url(r'^volunteers/', 'volunteers', name='frontend_volunteers'),
+                       url(r'^position/(?P<pos_id>\d+)/', 'position', name='frontend_position'),
                        url(r'^clubs/', 'clubs', name='frontend_clubs'),
                        url(r'^contact/', 'contact', name='frontend_contact')
 )
