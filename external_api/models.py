d00a2c55e8c64a6cbd7ff311b30257d4e9e82b1d
Keegan Parker <keegandparker@gmail.com>
2015-02-01 04:32:21 -0500

    some external apis

--- external_api/models.py
+++ external_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Faculty(models.Model):
+    desc = models.TextField()
+
+    def as_json(self):
+    	return {'id': self.pk,
+    			'desc': self.desc}
+
+class Student(models.Model):
+    quest_id = models.TextField(primary_key=True)
+    faculty_id = models.ForeignKey(Faculty, related_name="student_faculty")
+    term = models.CharField(max_length=2)
+
+    def as_json(self):
+    	return {'quest_id': self.quest_id,
+    			'faculty': Faculty.objects.filter(pk=self.faculty_id)[0].as_json(),
+    			'term': self.term}
